e8fbe195f608fb7c4fa0b6f41c1228a96984b0a8
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-10-11 16:21:13 -0300

    Consertei o background e o terreno e adicionei a animação de pulo

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,6 +13,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animation.play("jump_preparation")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,6 +24,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if is_on_floor():
+		if animation.animation == 'falling':
+			animation.play('landing_animation') # Se estava caindo, toca a animação de aterrissagem
+		elif animation.animation != "landing_animation" or not animation.is_playing():
+			if direction != 0:
+				animation.flip_h = (direction < 0)
+				animation.play("run")
+			else:
+				animation.play("idle")
+		
 		if direction > 0:
 			animation.flip_h = false
 			animation.play('run')
@@ -32,7 +42,13 @@ func _physics_process(delta: float) -> void:
 		else:
 			animation.play('idle')
 	else:
-		pass		
-		
+		if animation.animation == 'jump_preparation' and animation.is_playing():
+		 	# Deixa a animação de preparação terminar.
+			pass		
+		elif velocity.y < 0:
+			animation.play("flying_up")
+		elif velocity.y >= 0:
+			animation.play("falling")
+			
 	move_and_slide()
 	

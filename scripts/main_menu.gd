82ce2f4eabc033c933e965c4c00b36e04272110c
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-12-07 11:51:06 -0300

    Criei um menu de pause

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,12 +4,24 @@ extends Control
 @onready var settings_menu: Panel = $SettingsMenu
 @onready var click_audio: AudioStreamPlayer = $ClickAudio
 
+@export var is_pause_menu := false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	main_buttons.visible = true
 	settings_menu.visible = false
-	get_node("MainButtons/Play").grab_focus()
+	
+	# MENU PRINCIPAL: tenta focar o botão Play
+	if not is_pause_menu:
+		var play_button = get_node_or_null("MainButtons/Play")
+		if play_button:
+			play_button.grab_focus()
+	else:
+		# PAUSE MENU: foca o botão Continue
+		var continue_button = get_node_or_null("MainButtons/Continue")
+		if continue_button:
+			continue_button.grab_focus()
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,7 +31,12 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	click_audio.play()	
-	get_tree().change_scene_to_file("res://scene/forest.tscn")
+	
+	if is_pause_menu:
+		get_tree().paused = false
+		visible = false
+	else:
+		get_tree().change_scene_to_file("res://scene/forest.tscn")
 
 
 func _on_settings_pressed() -> void:
